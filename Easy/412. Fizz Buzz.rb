229441b0d9eec88e5b09799a602ad1aa199ad2d7
Muhammad Nawzad <hama127n@gmail.com>
2023-01-31 10:42:43 +0300

    Updates the unconventional for loop to each

--- Easy/412. Fizz Buzz.rb
+++ Easy/412. Fizz Buzz.rb
@@ -2,7 +2,7 @@
 # @return {String[]}
 def fizz_buzz(n)
   answer = []
-  for i in 1..n do
+  (1..n).each { |i|
     if i % 3 == 0 && i % 5 == 0
       answer.push("FizzBuzz")
     elsif i % 3 == 0
@@ -12,7 +12,7 @@ def fizz_buzz(n)
     else
       answer.push(i.to_s)
     end
-  end
+  }
 
   answer
 end
